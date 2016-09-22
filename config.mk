1fac57b08fa43dbfe073e5c07e12d02f37026908
Botond Sipos <bsipos@nanoporetech.com>
2016-09-22 17:06:13 +0100

    Added input file parameters.

--- config.mk
+++ config.mk
@@ -1,14 +1,24 @@
 # General pipeline parameters:
+
 # Parent directory to pipeline workspace:
-WORKSPACE=FILL
-# Pipelien name:
-PIPELINE_NAME=FILL
+WORKSPACE=./
+# Pipeline name:
+PIPELINE_NAME=assembly-polish
 # Pipeline working directory:
 WDIR=$(WORKSPACE)/$(PIPELINE_NAME)
 # Results directory:
 RES=$(WDIR)/results
 # Pipeline git repo:
-REPO=FILL
+REPO=git@git.oxfordnanolabs.local:bsipos/ont-assembly-polish.git
 
 # Custom pipeline parameters:
 
+# Input files:
+
+NANOPORE_READS=
+ILLUMINA_READS_PAIR1=
+ILLUMINA_READS_PAIR2=
+
+
+# Number of cores to use for multithreaded applications:
+CORES=32
