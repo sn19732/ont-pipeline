8bd04e78b40a3453342b6a24730a905ac6fdc992
Botond Sipos <bsipos@nanoporetech.com>
2016-09-23 11:26:38 +0100

    BWA and pilon steps now work on racon contigs.

--- analysis.mk
+++ analysis.mk
@@ -44,11 +44,11 @@ BWA_BAM_PREFIX=$(WDIR)/bwa_aligned_reads
 BWA_BAM=$(BWA_BAM_PREFIX).bam
 
 bwa_align: $(BWA_BAM)
-$(BWA_BAM): $(CANU_CONTIGS) $(ILLUMINA_READS_PAIR1) $(ILLUMINA_READS_PAIR2)
+$(BWA_BAM): $(RACONN_CONTIGS) $(ILLUMINA_READS_PAIR1) $(ILLUMINA_READS_PAIR2)
 	@echo Indexing contigs.
-	@bwa index $(CANU_CONTIGS)
+	@bwa index $(RACON_CONTIGS)
 	@echo Aligning Illumina reads using BWA mem.
-	@bwa mem -t $(CORES) $(BWA_PARAMETERS) $(CANU_CONTIGS)  $(ILLUMINA_READS_PAIR1) $(ILLUMINA_READS_PAIR2)\
+	@bwa mem -t $(CORES) $(BWA_PARAMETERS) $(RACON_CONTIGS)  $(ILLUMINA_READS_PAIR1) $(ILLUMINA_READS_PAIR2)\
 		| samtools view -S -b -u - | samtools sort -@ $(CORES) - $(BWA_BAM_PREFIX)
 	@samtools index $(BWA_BAM)
 
@@ -57,6 +57,6 @@ $(BWA_BAM): $(CANU_CONTIGS) $(ILLUMINA_READS_PAIR1) $(ILLUMINA_READS_PAIR2)
 PILON_CONTIGS=$(WDIR)/pilon.contigs.fasta
 
 pilon_correct: $(PILON_CONTIGS)
-$(PILON_CONTIGS): $(CANU_CONTIGS) $(BWA_BAM)
+$(PILON_CONTIGS): $(RACON_CONTIGS) $(BWA_BAM)
 	@echo Correcting contigs using pilon.
-	@pilon --threads $(CORES) --genome $(CANU_CONTIGS) --bam $(BWA_BAM) --outdir $(WDIR) --output pilon.contigs $(PILON_PARAMETERS)
+	@pilon --threads $(CORES) --genome $(RACON_CONTIGS) --bam $(BWA_BAM) --outdir $(WDIR) --output pilon.contigs $(PILON_PARAMETERS)
