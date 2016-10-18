c602a84607297e9481200691afad84dcb6e1bf30
Botond Sipos <Botond.Sipos@nanoporetech.com>
2016-10-18 13:29:08 +0100

    Template script update.

--- scripts/_template_script.py
+++ scripts/_template_script.py
@@ -2,22 +2,13 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import sys
-# Script meant to be run from pipeline repo, append that to path:
-sys.path.append('./')
 
+# Parse command line arguments:
+parser = argparse.ArgumentParser(
+    description='Template script.')
+parser.add_argument(
+    '-i', metavar='input', type=str, help="Input.")
 
-def parse_arguments():
-    """
-    Parse command line arguments.
-    """
-    parser = argparse.ArgumentParser(
-        description='Awesome script.')
-    parser.add_argument(
-        '-i', metavar='input', type=str, help="Input.")
-
-    args = parser.parse_args()
-    return args
 
 if __name__ == '__main__':
-    args = parse_arguments()
+    args = parser.parse_args()
